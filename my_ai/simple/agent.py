23188ee82a94a2a0ac2a3f3333f95a262e1b4d0d
nathantgray <nathantgray@gmail.com>
2021-10-18 11:42:19 -0700

    making units smarter using a new class to keep track of them

--- my_ai/simple/agent.py
+++ my_ai/simple/agent.py
@@ -57,6 +57,96 @@ class MapState:
                 if self.bd[y][x].citytile is not None:
                     self.start_pos.append((y, x))
 
+    def get_resource_tiles(self, game_state):
+        resource_tiles: list[Cell] = []
+        for y in range(self.height):
+            for x in range(self.width):
+                cell = game_state.map.get_cell(x, y)
+                if cell.has_resource():
+                    resource_tiles.append(cell)
+        return resource_tiles
+
+
+class UnitControl:
+    def __init__(self, unit, player):
+        self.player = player
+        self.unit = unit
+        self.next_pos = None
+        self.target = None
+        self.tasks = []  # stack of tasks
+        self.city = None
+
+    def update(self, assignment, actions):
+        self.city = assignment[self.unit.id]
+        resource_tiles = get_resource_tiles(game_state)
+        unit = self.unit
+        actions.append(annotate.sidetext(f"start_{unit.id}"))
+        if unit.is_worker() and unit.can_act():
+            if unit.get_cargo_space_left() > 0:
+                closest_resource_tile = get_closest_resource_tile(resource_tiles, unit, self.player)
+                if closest_resource_tile is not None:
+                    self.target = closest_resource_tile.pos
+                    actions = self.goto_target(actions)
+            else:
+                # if unit is a worker and there is no cargo space left, and we have cities, lets return to them
+                if len(self.player.cities) > 0:
+                    if unit.id in assignment.keys():
+                        actions.append(annotate.sidetext(f"send {unit.id}"))
+                        dest_city = assignment[unit.id]
+                        dest = get_closest_obj(unit.pos, dest_city.citytiles).pos
+                        move_dir = unit.pos.direction_to(dest)
+                        if dest_city.get_light_upkeep() < dest_city.fuel and self.player.city_tile_count < 2:  # build new city
+                            if unit.can_build(game_map=game_state.map):
+                                actions.append(unit.build_city())
+                            else:
+                                actions.append(unit.move(opposite_dir(move_dir)))
+                        else:
+                            actions.append(unit.move(move_dir))
+                    else:
+                        actions.append(annotate.sidetext("close_city"))
+                        closest_city_tile = get_closest_city_tile(unit, self.player)
+                        if closest_city_tile is not None:
+                            city = self.player.cities[closest_city_tile.cityid]
+                            dest = closest_city_tile.pos
+                            move_dir = unit.pos.direction_to(dest)
+                            if city.get_light_upkeep() < city.fuel:  # build new city
+                                if unit.can_build(game_map=game_state.map):
+                                    actions.append(unit.build_city())
+                                else:
+                                    actions.append(unit.move(opposite_dir(move_dir)))
+                            else:
+                                # print(closest_city_tile)
+                                actions.append(annotate.line(
+                                    dest.x, dest.y,
+                                    unit.pos.x, unit.pos.y
+                                ))
+
+                                actions.append(unit.move(move_dir))
+        pass
+
+    def set_target(self):
+        pass
+
+    def goto_target(self, actions):
+        clear_dir = [DIRECTIONS.NORTH, DIRECTIONS.EAST, DIRECTIONS.SOUTH, DIRECTIONS.WEST]
+        move_dir = self.unit.pos.direction_to(self.target)
+        self.next_pos = self.unit.pos.translate(move_dir, 1)
+        next_cell = game_state.map.get_cell_by_pos(self.next_pos)
+        while next_cell.citytile is not None:
+            if next_cell.citytile.cityid != self.city.cityid:
+                clear_dir.remove(move_dir)
+                move_dir = clear_dir[0]
+                self.next_pos = self.unit.pos.translate(move_dir, 1)
+                next_cell = game_state.map.get_cell_by_pos(self.next_pos)
+
+        actions.append(annotate.line(
+            self.target.x, self.target.y,
+            self.unit.pos.x, self.unit.pos.y
+        ))
+        actions.append(self.unit.move(move_dir))
+        return actions
+
+
 
 def setup(observation, game_state):
     ### Do not edit ###
@@ -72,7 +162,8 @@ def setup(observation, game_state):
     return actions
 
 
-def get_resource_tiles(width, height, game_state):
+def get_resource_tiles(game_state):
+    width, height = game_state.map.width, game_state.map.height
     resource_tiles: list[Cell] = []
     for y in range(height):
         for x in range(width):
@@ -142,17 +233,17 @@ def get_closest_obj(pos, obj_list):
     return closest_obj
 
 
-
 def delegate(player):
     assignment = {}
-    free_units = player.units
+    free_units = player.units.copy()
     for city in player.cities.values():
         for city_tile in city.citytiles:
-            closest_unit = get_closest_obj(city_tile.pos, free_units)
-            free_units.remove(closest_unit)
-            assignment[closest_unit] = city
-    return assignment
+            if len(free_units) > 0:
+                closest_unit = get_closest_obj(city_tile.pos, free_units)
+                free_units.remove(closest_unit)
+                assignment[closest_unit.id] = city
 
+    return assignment
 
 
 def agent(observation, configuration):
@@ -174,11 +265,8 @@ def agent(observation, configuration):
     opponent = game_state.players[(observation.player + 1) % 2]
     width, height = game_state.map.width, game_state.map.height
 
-    resource_tiles = get_resource_tiles(width, height, game_state)
+    resource_tiles = get_resource_tiles(game_state)
 
-
-
-    assignment = delegate(player)
 
     for city in player.cities.values():
         for city_tile in city.citytiles:
@@ -187,55 +275,21 @@ def agent(observation, configuration):
                 y = city_tile.pos.y
                 actions.append(annotate.sidetext(f"{x}-{y}-{city_tile.can_act()}"))
                 actions.append(annotate.circle(x, y))
-                if len(player.units)+1 < player.city_tile_count:
+                if len(player.units) < player.city_tile_count:
                     actions.append(city_tile.build_worker())
                 else:
                     actions.append(city_tile.research())
-
+    assignment = delegate(player)
+    for u_id, city in assignment.items():
+        actions.append(annotate.sidetext(f"{u_id}->{city.cityid}"))
                 # city_tile.build_worker()
-    # we iterate over all our units and do something with them
-    for unit in player.units:
-        if unit.is_worker() and unit.can_act():
-            if unit.get_cargo_space_left() > 0:
-                closest_resource_tile = get_closest_resource_tile(resource_tiles, unit, player)
-                if closest_resource_tile is not None:
-                    actions.append(annotate.line(
-                        closest_resource_tile.pos.x, closest_resource_tile.pos.y,
-                        unit.pos.x, unit.pos.y
-                    ))
-                    actions.append(unit.move(unit.pos.direction_to(closest_resource_tile.pos)))
-            else:
-                # if unit is a worker and there is no cargo space left, and we have cities, lets return to them
-                if len(player.cities) > 0:
-                    if unit in assignment.keys():
-                        dest = assignment[unit].pos
-                        move_dir = unit.pos.direction_to(dest)
-                        if city.get_light_upkeep() < city.fuel:  # build new city
-                            if unit.can_build(game_map=game_state.map):
-                                actions.append(unit.build_city())
-                            else:
-                                actions.append(unit.move(opposite_dir(move_dir)))
-                        else:
-                            actions.append(unit.move(move_dir))
-                    else:
-                        closest_city_tile = get_closest_city_tile(unit, player)
-                        if closest_city_tile is not None:
-                            city = player.cities[closest_city_tile.cityid]
-                            dest = closest_city_tile.pos
-                            move_dir = unit.pos.direction_to(dest)
-                            if city.get_light_upkeep() < city.fuel:  # build new city
-                                if unit.can_build(game_map=game_state.map):
-                                    actions.append(unit.build_city())
-                                else:
-                                    actions.append(unit.move(opposite_dir(move_dir)))
-                            else:
-                                # print(closest_city_tile)
-                                actions.append(annotate.line(
-                                    dest.x, dest.y,
-                                    unit.pos.x, unit.pos.y
-                                ))
 
-                                actions.append(unit.move(move_dir))
+    u_agents = {}
+    for unit in player.units:
+        if unit.id not in u_agents.keys():
+            u_agents[unit.id] = UnitControl(unit, player)
+    for uid, unit_ctr in u_agents.items():
+        unit_ctr.update(assignment, actions)
 
 
 
